14a780c851ab0d2d583e4401abc978a7742e6df0
Graham Rogers <grogers@Grahams-MacBook-Air.local>
2017-10-11 01:41:32 -0500

    changing database config

--- src/config/database.example.js
+++ src/config/database.example.js
@@ -1,3 +1,5 @@
 // looks like 'mongodb://<user>:<pass>@<server>:<port>/<db>'
-export const database = 'mongodb://<user>:<pass>@<server>:<port>/<db>';
-export const secret = 'your secret passphrase goes here';
+export default {
+    database: 'mongodb://<user>:<pass>@<server>:<port>/<db>',
+    secret: 'your secret passphrase goes here'
+}
